5347c3fefb239e94d470e56b1d27f5afc1317c49
Janek1010 <janekdomozych99@gmail.com>
2023-02-22 18:39:12 +0100

    1.3

---
+++ ExercicesFromBook/1.3/src/pl/janek/Main.java
@@ -0,0 +1,8 @@
+package pl.janek;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("Pierwiastek kwadratowy z liczby π wynosi: %2.2f", Math.sqrt(Math.PI));
+    }
+}
